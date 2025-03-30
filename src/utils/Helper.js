e535373ae0a3bd3b521209071eb52f1fb9255f20
Abhishek kumar <india52736628@gmail.com>
2025-03-30 17:50:13 +0530

    add_firebase

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1,8 +1,7 @@
 export const bg_img = "https://assets.nflxext.com/ffe/siteui/vlv3/42a0bce6-fc59-4c1c-b335-7196a59ae9ab/web/IN-en-20250303-TRIFECTA-perspective_d5f81427-d6cf-412d-8e86-2315671b9be1_small.jpg"
 export const Netflix_logo = "https://pngimg.com/uploads/netflix/netflix_PNG31.png"
 export const profile_Img = "https://imgs.search.brave.com/49ogd4rOP14ONhT6mrJ7zWxdIklksiQ-C_veJs54ha4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvOTM4/NzA5MzYyL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtZ2lybC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VVFH/WHBlaUxySTc4bk8x/QjlwZVVuMEQwZkNT/UnJtLUo4eG9oTVdH/Mkxtcz0"
-export const API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTEzNzdiMTMyN2VkN2ZlOWRhZjg4NjliODk4ODNmZiIsIm5iZiI6MTc0MjQ0OTA3NC4zNywic3ViIjoiNjdkYmE5YjIwZDMzOWJiNzE0N2Y5YjQ2Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.J6Lp48WpLMs5sLz3gNkKhl83TMmDPLxcEXYuWB_ShHE"
-export const API_KEY = "f11377b1327ed7fe9daf8869b89883ff"
+
 export const options = {
     method: 'GET',
     headers: {
