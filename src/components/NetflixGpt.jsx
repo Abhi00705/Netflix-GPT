7ea3272bb6b2b2f87bd62f67c96bab46becc425a
Abhishek kumar <india52736628@gmail.com>
2025-03-30 17:43:11 +0530

    env_added

--- src/components/NetflixGpt.jsx
+++ src/components/NetflixGpt.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { bg_img } from '../utils/Helper';
 import { useSelector } from 'react-redux';
 import {lang} from "../utils/lang";
@@ -10,9 +10,14 @@ import {lang} from "../utils/lang";
 const NetflixGpt = () => {
   const[lang1, setLang1] = useState();
  const language = useSelector((state)=>state.lang.langStatus);
- console.log(language);
-// console.log(lang[language].search, "checking");
  
+ const searchText = useRef(null);
+
+ const handleGPTsearch = (e)=>{
+  e.preventDefault();
+  console.log(searchText.current.value);
+ }
+
  
 
  
@@ -23,11 +28,15 @@ const NetflixGpt = () => {
    
     <form className='absolute bg-black top-52 left-1/3 p-1 w-1/3'>
       <input 
+        ref={searchText}
         type="text" 
         placeholder={lang[language].placeHolder}
         className="bg-white p-2 w-[80%]"
         />
-      <button className="bg-purple-800 text-white p-2 w-[20%] ">{lang[language].search}</button>
+      <button 
+        className="bg-purple-800 text-white p-2 w-[20%] "
+        onClick={handleGPTsearch}
+      >{lang[language].search}</button>
     </form>
     </>
   )
